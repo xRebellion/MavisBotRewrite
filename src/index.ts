bbaa673785bdba2eb03e8cc7fc76a1068e4bd636
Daniel- <danielyg2904@gmail.com>
2022-08-23 00:29:40 +0700

    finishing base index ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
-import { Client, Partials, IntentsBitField } from "discord.js";
+import { Client, Partials, IntentsBitField, ActivityType } from "discord.js";
+import winston from "winston";
+import { discordConfig } from "./config/discord";
+
+const logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.simple(),
+    transports: [
+        new winston.transports.Console(),
+    ],
+});
+
 const client = new Client({
     intents: [IntentsBitField.Flags.GuildVoiceStates, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.Guilds],
     partials: [Partials.Message, Partials.Channel, Partials.Reaction]
 });
+
+client.on('ready', () => {
+    client.user?.setPresence({ activities: [{ name: 'paint dry', type: ActivityType.Watching }], status: 'online' });
+    logger.info("Ready~");
+});
+
+client.login(discordConfig.TOKEN);
+
+
+
