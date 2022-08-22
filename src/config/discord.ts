f391d1e570bd4858ef26045c25114176ba590d0f
Daniel- <danielyg2904@gmail.com>
2022-08-23 00:28:15 +0700

    edit config

---
+++ src/config/discord.ts
@@ -0,0 +1,4 @@
+export const discordConfig = {
+    CLIENT_ID: process.env.MAVIS_CLIENT_ID ? process.env.MAVIS_CLIENT_ID : "",
+    TOKEN: process.env.MAVIS_BOT_TOKEN ? process.env.MAVIS_BOT_TOKEN : "",
+};
---
+++ src/config/youtube.ts
@@ -0,0 +1,6 @@
+export const youtubeConfig = {
+    API_URL: "https://www.googleapis.com/youtube/v3/search",
+    PLAYLIST_API_URL: "https://www.googleapis.com/youtube/v3/playlistItems",
+    VIDEO_URL: "https://www.youtube.com/watch",
+    PLAYLIST_URL: "https://www.youtube.com/playlist"
+};
